fe62aebf88556ebbe597e32b1ac9fe42448948eb
p1nkoS <96621552+p1nkoS@users.noreply.github.com>
2026-05-23 19:56:28 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CategoryPage from "./pages/CategoryPage";
 import ProductDetail from "./pages/ProductDetail";
 import AdminLogin from "./pages/AdminLogin";
 import AdminLeads from "./pages/AdminLeads";
-import AuthCallback from "./pages/AuthCallback";
 import { Toaster } from "./components/ui/sonner";
 import GlassCursor from "./components/GlassCursor";
 
 function AppRouter() {
-  const location = useLocation();
-  // CRITICAL: handle Emergent OAuth callback synchronously before normal routes
-  // REMINDER: DO NOT HARDCODE THE URL, OR ADD ANY FALLBACKS OR REDIRECT URLS, THIS BREAKS THE AUTH
-  if (location.hash && location.hash.includes("session_id=")) {
-    return <AuthCallback />;
-  }
   return (
     <Routes>
       <Route path="/" element={<Home />} />
